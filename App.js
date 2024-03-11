cd4e3261e66d0d935801651bbe576876d000e3fd
Eliton Camargo <camargoliveira@gmail.com>
2024-03-11 12:04:46 -0300

    Inserindo imagem

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.nome}>Eliton</Text>
+      <Text style={styles.cargo}>Professor</Text>
+      <Image
+        source='https://a.cdn-hotels.com/gdcs/production4/d1033/684b46ee-1e9d-452d-8851-3fc7e69ed45f.jpg'
+        style={styles.img}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nome:{
+    fontSize: 22,
+    color: '#555'
+  },
+  cargo:{
+    fontSize: 15,
+    color: '#ccc'
+  },
+  img:{
+    width:400,
+    height:300
+  }
 });
