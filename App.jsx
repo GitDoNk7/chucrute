6d3c16fc7b18781a69a89b3319cfc6f32a613c43
Eliton Camargo <camargoliveira@gmail.com>
2024-03-25 13:31:51 -0300

    Criando tela perfil

--- App.jsx
+++ App.jsx
@@ -1,30 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image';
-import Avatar from './assets/avatar.jpg';
+// import Avatar from './assets/avatar.jpg';
+import Foto from './assets/albert-einstein.jpg';
 
 export default function App() {
 
   return (
     <View style={estilo.container}>
       <Image
-        source={Avatar}
+        source={Foto}
         style={estilo.avatar}
       />
+      <Text style={estilo.nome}>
+        Albert Einstein
+      </Text>
+      <Text style={estilo.sobre}>
+        Albert Einstein foi um físico teórico alemão, que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica quântica.
+      </Text>
     </View>
   );
 }
 
 const estilo = StyleSheet.create({
   container:{
-    backgroundColor: 'rgba(255,255,0,255)', 
+    backgroundColor: 'rgba(255,255,255,255)', 
     flex:1, 
-    justifyContent:'space-around', 
+    justifyContent:'center', 
     alignItems:'center'
   },
   avatar:{
     width:200,
-    height:200
+    height:200,
+    borderRadius:100,
+    borderWidth: 5,
+    borderTopColor: "#000",
+    /// borderBottomColor:"#ff0",   
+  },
+  nome:{
+    fontSize: 24,
+    marginTop: 30
+  },
+  sobre:{
+    fontSize: 18,
+    marginHorizontal: 25,
+    textAlign:'justify',
+    padding: 20,
+    backgroundColor: "#55555538",
+    width: 350,
+    marginTop:30,
+    borderRadius:10
   }
 }); 
 
