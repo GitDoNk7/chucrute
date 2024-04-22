6182e6db2ae3fc1225f070a414c13ed15bc84ffd
GitDoNk7 <nichrpereira@gmail.com>
2024-04-22 11:23:15 -0300

    primeiro commit

--- App.jsx
+++ App.jsx
@@ -3,53 +3,127 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image';
 // import Avatar from './assets/avatar.jpg';
 import Foto from './assets/albert-einstein.jpg';
+import Linguine from './assets/images.jpg';
 
 export default function App() {
 
   return (
     <View style={estilo.container}>
+
+      <View style={estilo.titulo}>
+
       <Image
         source={Foto}
-        style={estilo.avatar}
+        style={estilo.imgPerfil}
       />
-      <Text style={estilo.nome}>
+      <Text style={estilo.nomeLL}>
         Albert Einstein
       </Text>
-      <Text style={estilo.sobre}>
-        Albert Einstein foi um físico teórico alemão, que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica quântica.
-      </Text>
+      </View>
+      <Image
+        source={Linguine}
+        style={estilo.imgDosD}
+      />
+      <View style={estilo.nome}>
+        <Text style={estilo.txttitulo}>Nome</Text>
+      </View>
+      <View style={estilo.Texto1}>
+        <Text style={estilo.Texto2}>Albertinho</Text>
+      </View>
+      <View style={estilo.nome3}>
+        <Text style={estilo.txttitulo}>Decrição de perfil</Text>
+      </View>
+      <View style={estilo.Texto1}>
+        <Text style={estilo.Texto2}>Cadê as do job, papai? </Text>
+      </View>
+      <View style={estilo.nome3}>
+        <Text style={estilo.txttitulo}>Email</Text>
+      </View>
+      <View style={estilo.Texto1}>
+        <Text style={estilo.Texto2}>albertinhodosgame@hotmail.com</Text>
+      </View>
+      <br></br>
     </View>
+    
   );
 }
 
 const estilo = StyleSheet.create({
   container:{
     backgroundColor: 'rgba(255,255,255,255)', 
-    flex:1, 
-    justifyContent:'center', 
-    alignItems:'center'
+    flex:1
+    
   },
-  avatar:{
-    width:200,
-    height:200,
-    borderRadius:100,
-    borderWidth: 5,
-    borderTopColor: "#000",
-    /// borderBottomColor:"#ff0",   
+  titulo: {
+    fontSize: 20,
+    width: '100%', 
+    height: 60,
+    backgroundColor: '#222',
+    color: '#fff',
+    textAlign: 'center'
+  },
+  subtitulo: {
+    fontSize: 25,
+    width: '100%', 
+    height: 60,
+    textAlign: 'center',
+    marginTop: 10
+  },
+  imgPerfil: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginTop: 5,
+    marginLeft: 5
+  },
+  imgDosD: {
+    width: 250,
+    height: 250,
+    borderRadius: '100%',
+    marginTop: 10,
+    marginLeft: 90,
+    borderColor: 'black',
+    borderWidth: 2
+  },
+  nomeLL:{
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: -42,
+    color: 'white'
+
   },
   nome:{
-    fontSize: 24,
-    marginTop: 30
+    marginLeft: 60,
+    marginTop: 50
   },
-  sobre:{
-    fontSize: 18,
-    marginHorizontal: 25,
-    textAlign:'justify',
-    padding: 20,
-    backgroundColor: "#55555538",
-    width: 350,
-    marginTop:30,
-    borderRadius:10
+  nome2:{
+    marginLeft: 60,
+    marginTop: 10
+  },
+  nome3:{
+    marginLeft: 60,
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderTopColor : "blak",
+    marginRight: 50
+  },
+  txttitulo:{
+    fontFamily: "monospace",
+    fontSize: 15,
+    color: "gray",
+    marginRight: 60
+  },
+  Texto1:{
+    marginLeft: 70,
+    marginTop: 5,
+    marginRight: 60
+  },
+  Texto2:{
+    fontFamily: "monospace",
+    fontSize: 16,
+    marginRight: 20
   }
 }); 
 
